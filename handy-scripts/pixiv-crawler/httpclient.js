e5037bcad260a8ea159cfcea02872ed5a909d3b7
Lucien_X <coder89757@gmail.com>
2021-01-07 01:56:31 +0800

    chore: 增加必要的信息防止因依赖版本不同而出错

--- handy-scripts/pixiv-crawler/httpclient.js
+++ handy-scripts/pixiv-crawler/httpclient.js
@@ -14,17 +14,17 @@ const defaultConfig = {
 		})
   },
   headers: {
-    'cache-control':' max-age=0',
-    'dnt':' 1',
-    'upgrade-insecure-requests':' 1',
-    'user-agent':' Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
-    'accept':' text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'sec-fetch-site':' none',
-    'sec-fetch-mode':' navigate',
-    'sec-fetch-user':' ?1',
-    'sec-fetch-dest':' document',
-    'referer':' https://www.pixiv.net/',
-    'accept-language': ' zh-CN,zh;q=0.9,en;q=0.8',
+    'cache-control':'max-age=0',
+    'dnt':'1',
+    'upgrade-insecure-requests':'1',
+    'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+    'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'sec-fetch-site':'none',
+    'sec-fetch-mode':'navigate',
+    'sec-fetch-user':'?1',
+    'sec-fetch-dest':'document',
+    'referer':'https://www.pixiv.net/',
+    'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
     'cookie': COOKIE,
   }
 };
